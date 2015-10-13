187efb7056aeca95dd734459d2c1096e096d1a3f
avikantz <avikantsainidbz@gmail.com>
2015-10-13 12:20:53 +0530

    Set up table view in swift.

--- LUG/ViewController.swift
+++ LUG/ViewController.swift
@@ -8,7 +8,7 @@
 
 import UIKit
 
-class ViewController: UIViewController, UITableViewDelegate {
+class ViewController: UIViewController, UITableViewDelegate, UITableViewDataSource {
 
 	@IBOutlet weak var tableView: UITableView!
 	
@@ -21,6 +21,19 @@ class ViewController: UIViewController, UITableViewDelegate {
 		super.didReceiveMemoryWarning()
 		// Dispose of any resources that can be recreated.
 	}
-
+	
+	func numberOfSectionsInTableView(tableView: UITableView) -> Int {
+		return 1
+	}
+	
+	func tableView(tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+		return 1
+	}
+	
+	func tableView(tableView: UITableView, cellForRowAtIndexPath indexPath: NSIndexPath) -> UITableViewCell {
+		let cell = tableView.dequeueReusableCellWithIdentifier("defaultCell", forIndexPath: indexPath)
+		return cell
+	}
+	
 }
 
